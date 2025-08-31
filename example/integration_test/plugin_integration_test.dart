a8a85c7e1ee59ea036ace4bf150d9cca910100dd
Martin Budinsky <martin.budinsky@wultra.com>
2025-08-31 22:01:32 +0200

    feat: Introduce test invocation

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -14,14 +14,55 @@
  * limitations under the License.
  */
 
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:async';
+
 import 'package:integration_test/integration_test.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'suites/utils_test.dart' as utils_suite;
+import 'suites/password_test.dart' as password_suite;
+import 'suites/powerauth_activation_test.dart' as activation_suite;
+import 'suites/powerauth_configure_test.dart' as configure_suite;
+import 'suites/powerauth_encryptor_test.dart' as encryptor_suite;
+import 'suites/powerauth_native_object_register_test.dart' as native_obj_suite;
+import 'suites/powerauth_password_test.dart' as powerauth_password_suite;
+import 'suites/powerauth_signature_test.dart' as signature_suite;
+import 'suites/powerauth_time_test.dart' as time_suite;
+import 'suites/powerauth_token_test.dart' as token_suite;
+import 'suites/powerauth_userinfo_test.dart' as userinfo_suite;
+import 'suites/powerauth_biometrics_automated_test.dart'
+    as biometrics_automated_suite;
 
-void main() {
+// A simple helper to wrap potentially async main functions.
+// Useful for when you need to call top-level async functions in the test's main.
+Future<void> _runSuite(Function suiteMain) async {
+  final Object? result = Function.apply(suiteMain, const []);
+
+  if (result is Future) {
+    await result;
+  }
+}
+
+Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    
-  });
+  // Proactively load the env so that tests run correctly from the IDE
+  // AND we don't have to import it from the example app or in each suite
+  try {
+    await dotenv.load(fileName: '.env');
+  } catch (_) {}
+
+  // Register (and run) all suites
+  await _runSuite(utils_suite.main);
+  await _runSuite(password_suite.main);
+  await _runSuite(configure_suite.main);
+  await _runSuite(activation_suite.main);
+  await _runSuite(encryptor_suite.main);
+  await _runSuite(native_obj_suite.main);
+  await _runSuite(powerauth_password_suite.main);
+  await _runSuite(signature_suite.main);
+  await _runSuite(time_suite.main);
+  await _runSuite(token_suite.main);
+  await _runSuite(userinfo_suite.main);
+  await _runSuite(biometrics_automated_suite.main);
 }
