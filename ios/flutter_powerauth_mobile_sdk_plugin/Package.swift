4bef43a98fef32663b98c73606c318fe809beb13
Samuel Manik <samuel.manik@wultra.com>
2026-08-13 10:25:37 +0200

    remove snapshot versions

--- ios/flutter_powerauth_mobile_sdk_plugin/Package.swift
+++ ios/flutter_powerauth_mobile_sdk_plugin/Package.swift
@@ -15,9 +15,8 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "FlutterFramework", path: "../FlutterFramework"),
-        // Mirrors the CocoaPods `PowerAuth2` dependency (`~> 2.0.0-rc2`).
-        // TODO: Update the dependency once PowerAuth SDK 2.0.0 is released.
-        .package(url: "https://github.com/wultra/powerauth-mobile-sdk", exact: "2.0.0-rc2"),
+        // Mirrors the CocoaPods `PowerAuth2` dependency (`~> 2.0.0`).
+        .package(url: "https://github.com/wultra/powerauth-mobile-sdk", exact: "2.0.0"),
     ],
     targets: [
         .target(
